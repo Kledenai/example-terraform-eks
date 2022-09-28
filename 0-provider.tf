79165087f8f87e3fdf86b986c59ec170db2cffd4
Master Shake <41597373+Kledenai@users.noreply.github.com>
2022-09-27 22:38:56 -0300

    replace provider

--- 0-provider.tf
+++ 0-provider.tf
@@ -1,7 +1,3 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
 terraform {
   required_providers {
     aws = {
@@ -9,4 +5,8 @@ terraform {
       version = "~> 3.0"
     }
   }
+}
+
+provider "aws" {
+  region = "us-east-1"
 }
